f3e6cb901e61274d6f1f11debe97be446b93b422
unknown <kb.coders@mail.ru>
2023-02-14 12:11:40 +0400

    .

--- JavaScript Algorithm tasks/February 9-10, 2023/05 ► Max character limit.js
+++ JavaScript Algorithm tasks/February 9-10, 2023/05 ► Max character limit.js
@@ -1,9 +1,45 @@
-var text = "abcdefghjklmno"
-var newText = ""
-var limit = +prompt("Enter character limit:", "10")
+// var text = "abcdefghjklmno"
+// var newText = ""
+// var limit = +prompt("Enter character limit:", "10")
+// for (var i = 0; i < limit; i++) {
+//     newText += text[i]
+// }
+// console.log(newText)
+// ◄—————————————————————————————————————————————————————►
+var text = "Lorem Ipsum Dolor Sit Amet"
+var charaCountsArr = []
+var charaCountsWithLetters = []
 
-for (var i = 0; i < limit; i++) {
-    newText += text[i]
+text = text.toLowerCase()
+
+for (var i=0; i<text.length; i++) {
+    var counter = 0
+    var letter = ""
+    for (var j=0; j<text.length; j++) {
+        if (text[j] == text[i]) {
+            counter++
+        }
+    }
+    charaCountsArr.push(counter)
+    if (text[i] == " ") {
+        var t = "space"
+        letter += t + counter
+    }
+    else {
+        letter += text[i] + counter
+    }
+    charaCountsWithLetters.push(letter)
 }
 
-console.log(newText)
+var k = Math.max(...charaCountsArr)
+
+// console.log(charaCountsArr)
+// console.log(charaCountsWithLetters)
+// console.log(k)
+
+for (var x=0; x<charaCountsArr.length; x++) {
+    if (charaCountsArr[x] == k) {
+        console.log(charaCountsWithLetters[x])
+        // break
+    }
+}
--- JavaScript Algorithm tasks/February 9-10, 2023/06 ► Anagrams.js
+++ JavaScript Algorithm tasks/February 9-10, 2023/06 ► Anagrams.js
@@ -5,11 +5,43 @@ var counter = 0
 // var firstText = "Hello lorem"
 // var secondText = "Hello ipsum"
 
+// var firstText = "Hello lorem"
+// var secondText = "Hello sit"
 
-for (var i = 0; i < firstText.length; i++) {
-    for (var j = 0; j < secondText.length; j++) {
-        if (firstText[i] == secondText[j]) {
+// var firstText = "Hello amet world"
+// var secondText = "Hello dolor"
+
+var words1 = firstText.split(" ")
+var words2 = secondText.split(" ")
+
+for (var i = 0; i < words1.length; i++) {
+    var counter = 0
+    for (var j = 0; j < words2.length; j++) {
+        if (words2[j] == words1[i]) {
             counter++
+            if (counter != 1) {
+                console.log("Phrases is not same!")
+                break
+            }
+            else {
+                console.log("Phrases is same!")
+            }
+        }
+    }
+}
+
+for (var i = 0; i < words2.length; i++) {
+    var counter = 0
+    for (var j = 0; j < words1.length; j++) {
+        if (words1[j] == words2[i]) {
+            counter++
+            if (counter != 1) {
+                console.log("Phrases is not same!")
+                break
+            }
+            else {
+                console.log("Phrases is same!")
+            }
         }
     }
 }
@@ -18,4 +50,9 @@ if (firstText.length != secondText.length) {
     console.log("First phrase character count: ", firstText.length)
     console.log("Second phrase character count: ", secondText.length)
     console.log("FALSE: ", "Phrase length is not equal!")
+}
+else {
+    console.log("First phrase character count: ", firstText.length)
+    console.log("Second phrase character count: ", secondText.length)
+    console.log("TRUE: ", "Phrase length is equal!")
 }
