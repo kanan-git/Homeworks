483844f4089bb2ffe6b543447bc2aff8d243c4d7
unknown <kb.coders@mail.ru>
2023-02-24 23:19:01 +0400

    update February 24, 2023

--- 03 ■► JS DOM/tasks/2023 → 02 → 22/01 ► single item slide.js
+++ 03 ■► JS DOM/tasks/2023 → 02 → 22/01 ► single item slide.js
@@ -1,9 +1,16 @@
 const prevBtn = document.querySelector(".btn-prev")
 const nextBtn = document.querySelector(".btn-next")
 const slides = document.querySelectorAll(".slide")
+const radios = document.querySelectorAll(".dots-radios")
 
 let currentIndex = 0
 let latestIndex = slides.length - 1
+let radioIndex = currentIndex
+
+// set first slide to position 0
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentIndex)}%)`
+})
 
 prevBtn.addEventListener("click", (() => {
     currentIndex === 0 ? (currentIndex = latestIndex) : currentIndex--
@@ -11,6 +18,9 @@ prevBtn.addEventListener("click", (() => {
     slides.forEach((slide, index) => {
         slide.style.transform = `translateX(${100 * (index - currentIndex)}%)`
     })
+    if (radioIndex == 0) {
+        
+    }
 }))
 nextBtn.addEventListener("click", () => {
     currentIndex === latestIndex ? (currentIndex = 0) : currentIndex++
