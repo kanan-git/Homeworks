af389d7ac306f48c495c595272a11f411f1c1ff0
unknown <kb.coders@mail.ru>
2023-02-28 12:34:11 +0400

    update February 28, 2023

--- 03 ■► JS DOM/tasks/2023 → 02 → 27/Task 1/01 ► simple animation loop.js
+++ 03 ■► JS DOM/tasks/2023 → 02 → 27/Task 1/01 ► simple animation loop.js
@@ -1,1 +1,2 @@
 // 01 => Creating a simple animation loop: Start by creating a basic animation loop using requestAnimationFrame() that moves an object across the screen.
+
--- 03 ■► JS DOM/tasks/2023 → 02 → 27/Task 2/02 ► bouncing ball.js
+++ 03 ■► JS DOM/tasks/2023 → 02 → 27/Task 2/02 ► bouncing ball.js
@@ -1,1 +1,18 @@
 // 02 => Creating a bouncing ball: Use the same basic animation loop to create a bouncing ball animation. Make the ball bounce back and forth across the screen, changing direction each time it hits a wall.
+
+const ball = document.querySelector(".container__ball")
+const testbtn1 = document.querySelector(".testbtn1")
+const testbtn2 = document.querySelector(".testbtn2")
+
+testbtn1.addEventListener("click", 
+() => {
+    ball.style.animation = `bouncingToRight 3s ease-in 0s 1 normal forwards`
+    console.log("working")
+}
+)
+testbtn2.addEventListener("click", 
+() => {
+    ball.style.animation = `riseToRight 3s ease-out 0s 1 normal forwards`
+    console.log("working")
+}
+)
