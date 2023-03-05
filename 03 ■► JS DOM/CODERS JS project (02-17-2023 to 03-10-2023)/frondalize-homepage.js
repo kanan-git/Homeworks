f230ce6df4c77aba3bbd677cb1b44fd556d32aaf
unknown <kb.coders@mail.ru>
2023-03-05 17:11:28 +0400

    Frondalize JS DOM project | some changes

--- 03 ■► JS DOM/CODERS JS project (02-17-2023 to 03-10-2023)/frondalize-homepage.js
+++ 03 ■► JS DOM/CODERS JS project (02-17-2023 to 03-10-2023)/frondalize-homepage.js
@@ -1,13 +1,21 @@
 // ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬
 // VALUES (const, let, var) start
-
+const logo = document.querySelector(".header__logo")
 // VALUES (const, let, var) end
 // ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬
 // FUNCTIONS start
-
+function visionMode(light, dark) {
+    light = `invert(0)`
+    dark = `invert(1)`
+    if (logo.style.filter != dark) {
+        logo.style.filter = dark
+    } else {
+        logo.style.filter = light
+    }
+}
 // FUNCTIONS end
 // ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬
 // ACTIONS (timers, event listeners) start
-
+logo.addEventListener("click", visionMode)
 // ACTIONS (timers, event listeners) end
 // ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬ ▬▬▬▬▬▬▬▬▬▬
