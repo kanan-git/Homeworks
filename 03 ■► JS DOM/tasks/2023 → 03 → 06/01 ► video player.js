409f56def39879982e9b8cbc619e73c409a0dfe6
unknown <kb.coders@mail.ru>
2023-03-08 14:42:12 +0400

    update March 8, 2023 at 14:42

--- 03 ■► JS DOM/tasks/2023 → 03 → 06/01 ► video player.js
+++ 03 ■► JS DOM/tasks/2023 → 03 → 06/01 ► video player.js
@@ -1,38 +1,48 @@
 // ------------ - - → | ALL VARIABLES
-var video = document.querySelector(".videoplayer__content")
-var volumeSlider = document.querySelector(".toolbar__volume--slider_input")
-var volumeZero = document.querySelector(".fa-volume-xmark")
-var volumeLow = document.querySelector(".fa-volume-low")
-var volumeHigh = document.querySelector(".fa-volume-high")
-var volumeButton = document.querySelector(".toolbar__volume--symbols")
-var toolBar = document.querySelector(".toolbar")
-var progressBar = document.querySelector(".videoplayer__progressbar")
-var progressionPos = document.querySelector(".videoplayer__progressbar--progression")
-var progressionDot = document.querySelector(".videoplayer__progressbar--position")
-var volInpValArr = []
-var prevTime = video.currentTime
-// var videoDuration = 155.225397
-var playbackSpeed = document.querySelector(".toolbar__speed")
-var ppBtn = document.querySelector(".toolbar__pp")
-var btnIconPLAY = document.querySelector(".fa-play")
-var btnIconPAUSE = document.querySelector(".fa-pause")
-var btnIconRESTART = document.querySelector(".fa-retweet")
-var skip = document.querySelectorAll("[data-skip]")
-var moreDots = document.querySelector(".toolbar__more")
-var moreDropdown = document.querySelector(".toolbar__more-dropdown")
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{
+    var video = document.querySelector(".videoplayer__content")
+    var volumeSlider = document.querySelector(".toolbar__volume--slider_input")
+    var volumeZero = document.querySelector(".fa-volume-xmark")
+    var volumeLow = document.querySelector(".fa-volume-low")
+    var volumeHigh = document.querySelector(".fa-volume-high")
+    var volumeButton = document.querySelector(".toolbar__volume--symbols")
+    var toolBar = document.querySelector(".toolbar")
+    var progressBar = document.querySelector(".videoplayer__progressbar")
+    var progressionPos = document.querySelector(".videoplayer__progressbar--progression")
+    var progressionDot = document.querySelector(".videoplayer__progressbar--position")
+    var volInpValArr = []
+    var prevTime = video.currentTime
+    // var videoDuration = 155.225397
+    var playbackSpeed = document.querySelector(".toolbar__speed")
+    var ppBtn = document.querySelector(".toolbar__pp")
+    var btnIconPLAY = document.querySelector(".fa-play")
+    var btnIconPAUSE = document.querySelector(".fa-pause")
+    var btnIconRESTART = document.querySelector(".fa-retweet")
+    var skip = document.querySelectorAll("[data-skip]")
+    var moreDots = document.querySelector(".toolbar__more")
+    var moreDropdown = document.querySelector(".toolbar__more-dropdown")
+    var endOfClip = video.duration
+    var fullScrnBtn = document.querySelector(".toolbar__expand")
+    var fullScrnExpand = document.querySelector(".fa-expand")
+    var fullScrnCollapse = document.querySelector(".fa-down-left-and-up-right-to-center")
+    var percentOfProgBar = 0
+    var touchScrnLayer = document.querySelector(".videoplayer__touchbuttons")
+    var backwardTSbtn = document.querySelector(".videoplayer__touchbuttons--backward")
+    var forwardTSbtn = document.querySelector(".videoplayer__touchbuttons--forward")
+    var pauseTSbtn = document.querySelector(".videoplayer__touchbuttons--pause")
+    var playTSbtn = document.querySelector(".videoplayer__touchbuttons--play")
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | STARTING VALUES
 {
     video.volume = volumeSlider.value
     video.playbackRate = 1
     volInpValArr.push(video.volume)
     volInpValArr.push(video.volume)
-    var percentOfProgBar = 0
     progressionPos.style.width = `${percentOfProgBar}%`
     progressionDot.style.left = `${percentOfProgBar}%`
-    var endOfClip = video.duration
 }
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | VOLUME PARAMETERS
 {
     volumeSlider.addEventListener("input", 
@@ -56,8 +66,8 @@ var moreDropdown = document.querySelector(".toolbar__more-dropdown")
             volumeHigh.classList.remove("hidden")
         }
     }
-)
-volumeButton.addEventListener("click", 
+    )
+    volumeButton.addEventListener("click", 
     () => {
         volInpValArr.shift()
         volInpValArr.push(video.volume)
@@ -87,9 +97,9 @@ volumeButton.addEventListener("click",
             volumeHigh.classList.remove("hidden")
         }
     }
-)
+    )
 }
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | PROGRESS BAR PARAMETERS
 {
     video.addEventListener("timeupdate", () => {
@@ -103,7 +113,7 @@ volumeButton.addEventListener("click",
             btnIconPAUSE.classList.add("hidden")
             btnIconPLAY.classList.add("hidden")
             btnIconRESTART.classList.remove("hidden")
-            console.log("VIDEO CLIP ENDED NOW")
+            // console.log("VIDEO CLIP ENDED NOW")
         }
         // video.ended event listener ...
     })
@@ -150,43 +160,107 @@ volumeButton.addEventListener("click",
         }
     )
 }
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | VIDEO PLAYBACK SPEED
-playbackSpeed.addEventListener("change", () => {
-    video.playbackRate = playbackSpeed.value
-})
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{
+    playbackSpeed.addEventListener("change", () => {
+        video.playbackRate = playbackSpeed.value
+    })
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | VIDEO PLAY/PAUSE
-ppBtn.addEventListener("click", () => {
-    if (video.paused == true) {
-        video.play()
-        btnIconPAUSE.classList.remove("hidden")
-        btnIconPLAY.classList.add("hidden")
-        btnIconRESTART.classList.add("hidden")
-    } else {
-        video.pause()
-        btnIconPAUSE.classList.add("hidden")
-        btnIconPLAY.classList.remove("hidden")
-        btnIconRESTART.classList.add("hidden")
+{
+    function pVp() {
+        if (video.paused == true) {
+            video.play()
+            btnIconPAUSE.classList.remove("hidden")
+            btnIconPLAY.classList.add("hidden")
+            btnIconRESTART.classList.add("hidden")
+        } else {
+            video.pause()
+            btnIconPAUSE.classList.add("hidden")
+            btnIconPLAY.classList.remove("hidden")
+            btnIconRESTART.classList.add("hidden")
+        }
+        if (video.currentTime == endOfClip) {
+            currentTime = 0
+            video.play()
+        }
     }
-    if (video.currentTime == endOfClip) {
-        currentTime = 0
-        video.play()
-    }
-})
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ppBtn.addEventListener("click", pVp)
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | MORE BUTTON
-moreDots.addEventListener("click", () => {
-    moreDropdown.classList.toggle("hidden")
-})
-video.addEventListener("mouseout", () => {
-    moreDropdown.classList.add("hidden")
-})
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{
+    moreDots.addEventListener("click", () => {
+        moreDropdown.classList.toggle("hidden")
+    })
+    video.addEventListener("mouseout", () => {
+        moreDropdown.classList.add("hidden")
+    })
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ------------ - - → | SKIP BUTTONS
-
-skip.forEach(btn => {
-    btn.addEventListener("click", updateTime)
-    console.log(Number(this.dataset.skip))
-  })
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{
+    skip.forEach(btn => {
+        btn.addEventListener("click", () => {
+            // console.log(Number(btn.dataset.skip))
+            video.currentTime += Number(btn.dataset.skip)
+        })
+    })
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ------------ - - → | TOUCHSREEN EFFECTS & BUTTONS
+{
+    // PLAY AND PAUSE BUTTONS ON CLICK
+    function TSpVp() {
+        if (video.paused == true) {
+            video.play()
+            btnIconPAUSE.classList.remove("hidden")
+            btnIconPLAY.classList.add("hidden")
+            btnIconRESTART.classList.add("hidden")
+            touchScrnLayer.classList.remove("hidden")
+            playTSbtn.classList.remove("hidden")
+            setTimeout(() => {
+                touchScrnLayer.classList.add("hidden")
+                playTSbtn.classList.add("hidden")
+            }, 50)
+        } else {
+            video.pause()
+            btnIconPAUSE.classList.add("hidden")
+            btnIconPLAY.classList.remove("hidden")
+            btnIconRESTART.classList.add("hidden")
+            touchScrnLayer.classList.remove("hidden")
+            pauseTSbtn.classList.remove("hidden")
+            setTimeout(() => {
+                touchScrnLayer.classList.add("hidden")
+                pauseTSbtn.classList.add("hidden")
+            }, 50)
+        }
+        if (video.currentTime == endOfClip) {
+            currentTime = 0
+            video.play()
+        }
+    }
+    video.addEventListener("click", TSpVp)
+    // SKIP FORWARD & BACKWARD ON DOUBLE CLICK SIDES
+    function TSfwNbw() {}
+    video.addEventListener("dblclick", () => {
+        console.log("TEST 123")
+    })
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ------------ - - → | FULLSCREEN BUTTON
+{
+    fullScrnBtn.addEventListener("click", () => {
+        video.requestFullscreen()
+        // fullScrnExpand.classList.toggle("hidden")
+        // fullScrnCollapse.classList.toggle("hidden")
+        if (video.requestFullscreen) {
+            console.log("fullscreen mode")
+        } else {
+            console.log("window mode")
+        }
+    })
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ 03 ■► JS DOM/tasks/2023 → 03 → 07/01 ► music player.js
@@ -0,0 +1,31 @@
+// ⏵⏸
+
+// dragable progressbar feature add
+// volume slider add
+// dynamic img, dynamic txt fetch data from folder into array
+
+var audio = document.querySelector("audio")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================================================
+// CHECK LATER (ChatGPT)
+// Sure, I can explain the steps to create an audio waveform effect without writing any code.
+// Import your audio file: The first step is to import the audio file you want to create a waveform effect for into your chosen audio editing software. Make sure the audio file is of good quality and has a clear sound.
+// Analyze the audio file: Use the software's audio analysis tools to analyze the audio file and identify the key frequencies, pitch changes, and other audio characteristics that will help you create the waveform effect.
+// Create a new layer: Create a new layer on top of the audio file layer, and make sure it is the same length as the audio file.
+// Draw the waveform: Using your software's drawing tools, draw a waveform on the new layer. This waveform should match the audio file's sound and timing.
+// Apply effects: Once you have drawn the waveform, you can apply various effects to it to give it a more realistic look. For example, you can add a gradient fill, drop shadow, or other visual effects to make the waveform stand out.
+// Adjust the waveform: Adjust the waveform as needed to ensure it matches the audio file's sound and timing. You may need to make minor tweaks to the waveform to get it just right.
+// Export the waveform: Finally, export the waveform as a separate file so that you can use it in your project. You can also add the waveform to your original audio file to create a more dynamic and engaging effect.
