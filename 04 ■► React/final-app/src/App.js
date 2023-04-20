5654c62892140b760d42e82240149fec2f627a82
unknown <kb.coders@mail.ru>
2023-04-20 13:26:23 +0400

    April 20, 2023 at 13:26

--- 04 ■► React/final-app/src/App.js
+++ 04 ■► React/final-app/src/App.js
@@ -1,58 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+// import { Counter } from './features/counter/Counter';
+// import './App.css';
 
 function App() {
+
+  function testingJsonServer() {
+    fetch('https://localhost:3001/headerListItems')
+    .then(res => res.json())
+    .then(result => console.log(result))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="App" onClick={testingJsonServer}>
+      12345
     </div>
   );
 }
 
 export default App;
+
+// cd 04\ ■►\ React/final-app (for changing folder path for git bash command panel)
+// cd .. (back to previous folder if direct wrong path)
+// npm install json-server (for db.json in root folder)
+// npm start (for starting react live server on port 3000)
+// npx json-server --watch db.json --port 3001 (for starting json server on port 3001)
+// update github: 1) git add .; 2) git commit -m "comment here"; 3) git push origin master
