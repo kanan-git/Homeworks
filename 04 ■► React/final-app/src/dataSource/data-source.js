35fca7a11eeca81d496ad602cab31d63dcec60ef
unknown <kb.coders@mail.ru>
2023-04-24 20:05:18 +0400

    update

---
+++ 04 ■► React/final-app/src/dataSource/data-source.js
@@ -0,0 +1,15 @@
+export const dataSource = {
+    english: {
+        header: ["homepage", "products", "about us", "contact"],
+        footer: {},
+        homepage: {}
+    },
+
+    azerbaijani: {
+        // copy all to here and translate
+    },
+
+    turkish: {
+        // copy all to here and translate
+    }
+}
--- 04 ■► React/final-app/src/index.js
+++ 04 ■► React/final-app/src/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import './index.css';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
-    // <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    // </Provider>
-  // </React.StrictMode>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
+
+// ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+// cd 04\ ■►\ React/final-app (for changing folder path for git bash command panel)
+// cd .. (back to previous folder if direct wrong path)
+// npx create-react-app name-here --template redux
+// npm install axios (install axios library) && import axios from 'axios'
+// npm start (for starting react live server on port 3000)
+// update github: 1) git add .; 2) git commit -m "comment here"; 3) git push origin master
+// ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
