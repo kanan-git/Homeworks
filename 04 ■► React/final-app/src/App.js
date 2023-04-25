fb3180325591e8a98c7f1705b00f8f02653e41c9
unknown <kb.coders@mail.ru>
2023-04-25 10:47:36 +0400

    update April 25, 2023 at 10:44

--- 04 ■► React/final-app/src/App.js
+++ 04 ■► React/final-app/src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      test 12345
     </div>
   );
 }
--- 04 ■► React/final-app/src/components/Header/Header.jsx
+++ 04 ■► React/final-app/src/components/Header/Header.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import s from './index.module.css'
+import {dataSource} from '../../dataSource/data-source'
+import {useState, useEffect} from 'react'
 
 function Header() {
+    const [menuElements, setMenuElements] = useState([])
+    useEffect(
+        () => {
+            setMenuElements(dataSource.english.header)
+        }, []
+    )
+    console.log(menuElements)
     return (
         <header className={s.header}>
-            <div className={s.header__list}>
-                <button className={s.header__list_buttons}> Homepage </button> {/* temporary static buttons for reference */}
-
-                <button className={s.header__list_buttons}> Products </button> {/* temporary static buttons for reference */}
-                <button className={s.header__list_buttons}> About us </button> {/* temporary static buttons for reference */}
-                <button className={s.header__list_buttons}> Contact </button> {/* temporary static buttons for reference */}
+            {/* top side of the header as navbar */}
+            <nav className={s.header__list}>
+                {
+                    menuElements.map(
+                        ((element, index) => <button key={index} className={s.header__list_buttons}> {element} </button>)
+                    )
+                }
+            </nav>
+            {/* bottom main side of the header contains logo, searchbar, language bar, userpanel */}
+            {/* userpanel in dropdown: switch light/dark, if quest - sign in & register, if signed in - favorites, cart, log out */}
+            <div className={s.header__main}>
+                <img src="#" alt="." className={s.header__main_logo} /> {/* temporary static elements for reference */}
+                <span className={s.header__main_}></span> {/* temporary static elements for reference */}
+                <span className={s.header__main_}></span> {/* temporary static elements for reference */}
+                <span className={s.header__main_}></span> {/* temporary static elements for reference */}
+                <span className={s.header__main_}></span> {/* temporary static elements for reference */}
+                <span className={s.header__main_}></span> {/* temporary static elements for reference */}
             </div>
         </header>
     )
