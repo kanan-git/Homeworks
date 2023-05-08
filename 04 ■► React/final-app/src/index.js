0422738d8e2ea30fa662b52ff689848aa3070776
unknown <kb.coders@mail.ru>
2023-05-08 14:02:38 +0400

    work in progress - pages, components, routing

--- 04 ■► React/final-app/src/index.js
+++ 04 ■► React/final-app/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 // import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client';
 // import { Provider } from 'react-redux';
 // import { store } from './app/store';
 import App from './App';
+// import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  // <Provider store={store}>
-    <App />
-  // </Provider>
+  <StrictMode>
+    {/* <Provider store={store}> */}
+      {/* <BrowserRouter> */}
+        {/* <Routes> */}
+          {/* <Route path="/" element={<App />}> */}
+            {/* <Route path="/" element={<Homepage />} /> */}
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/products/view" element={<ItemView />} /> */}
+            <App />
+          {/* </Route> */}
+        {/* </Routes> */}
+      {/* </BrowserRouter> */}
+    {/* </Provider> */}
+  </StrictMode>
 );
 
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
@@ -21,4 +33,5 @@ root.render(
 // npm install axios (install axios library) && import axios from 'axios'
 // npm start (for starting react live server on port 3000)
 // update github: 1) git add .; 2) git commit -m "comment here"; 3) git push origin master
+// npm install react-router-dom
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
